84fbe1acc53336fd7a03b46200d7aee28efbfcba
Gabriel Maciel <g.maciel@buenosaires.gob.ar>
2023-05-19 14:48:02 -0300

    Add canvas to rotate feature and download button

--- src/routes/ImagePanel.svelte
+++ src/routes/ImagePanel.svelte
@@ -1,10 +1,56 @@
-<script>
+<script lang="ts">
 	import ButtonBar from './ButtonBar.svelte';
+	import { onMount } from 'svelte';
+
 	const buttons = [{ icon: 'icono1' }, { icon: 'icono2' }, { icon: 'icono3' }, { icon: 'icono4' }];
-	const image =
-		'https://fastly.picsum.photos/id/180/200/200.jpg?hmac=YtJJ-CeQThqv_K6NzUnKS6Q8-tjxUVkSKeDsStrjEyM';
+	const image = 'https://fastly.picsum.photos/id/180/200/200.jpg?hmac=YtJJ-CeQThqv_K6NzUnKS6Q8-tjxUVkSKeDsStrjEyM';
 
 	let currentRotation = 0;
+	let canvas: HTMLCanvasElement;
+	let ctx: CanvasRenderingContext2D | null;
+
+	onMount(() => {
+		if (!canvas) throw new Error("Canvas no está definido");
+
+		ctx = canvas.getContext('2d');
+		if (!ctx) throw new Error("Contexto de canvas no está definido");
+
+		let img = new Image();
+		img.crossOrigin = 'Anonymous'; // Añade esta línea
+		img.onload = drawRotated.bind(img);
+		img.src = image;
+	});
+
+	function drawRotated(this: HTMLImageElement) {
+		if (!ctx || !canvas) throw new Error("Contexto de canvas no está definido");
+
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.save();
+		ctx.translate(canvas.width / 2, canvas.height / 2);
+		ctx.rotate(currentRotation * Math.PI / 180);
+		ctx.drawImage(this, -this.width / 2, -this.height / 2);
+		ctx.restore();
+	}
+
+	function rotate() {
+    currentRotation += 90;
+    if (canvas && ctx) {
+        let img = new Image();
+        img.crossOrigin = 'Anonymous'; 
+        img.onload = drawRotated.bind(img);
+        img.src = image;
+    }
+}
+
+
+	function download() {
+		if (!canvas) throw new Error("Canvas no está definido");
+
+		let link = document.createElement('a');
+		link.download = 'download.png';
+		link.href = canvas.toDataURL();
+		link.click();
+	}
 </script>
 
 <main class="image-panel">
@@ -20,10 +66,11 @@
 	<div class="right-card">
 		<div class="card-image">
 			<!-- svelte-ignore a11y-img-redundant-alt -->
-			<img src={image} alt="Right Image" style="transform: rotate({currentRotation}deg);" />
+			<canvas bind:this={canvas} width="200" height="200" class="canvas-image"></canvas>
 		</div>
 		<div class="card-footer">
-			<button class="btn" on:click={() => currentRotation += 90}>Girar</button>
+			<button class="btn" on:click={rotate}>Girar</button>
+			<button class="btn" on:click={download}>Descargar</button>
 		</div>
 	</div>
 </main>
@@ -56,7 +103,8 @@
 		padding: 10px;
 	}
 
-	.card-image > img {
+	.card-image > img,
+	.canvas-image {
 		max-width: 100%;
 		max-height: 100%;
 		object-fit: contain;
